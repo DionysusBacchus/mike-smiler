7d6a2333a400f894b23db2a78c60fa829d9f5bf8
unknown <xkhrysts@ad.fi.muni.cz>
2024-12-03 14:30:05 +0100

    add basic setup

--- sketch.js
+++ sketch.js
@@ -21,6 +21,9 @@ function help(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();  // Create audio context
+  gainNode = audioContext.createGain();  // Create a gain node for volume control
+  gainNode.connect(audioContext.destination);  // Connect to speakers
 
   rectMode(CENTER);
 
@@ -43,6 +46,24 @@ function setup() {
   }
   fileInput = createFileInput(handleFile);
   fileInput.hide();
+}
+
+function playMIDI(midi) {
+  const now = audioContext.currentTime;
+
+  midi.tracks.forEach((track) => {
+    track.notes.forEach((note) => {
+      const frequency = midiToFreq(note.midi); // Convert MIDI note to frequency
+
+      let osc = audioContext.createOscillator();  // Create an oscillator
+      osc.type = 'sine';  // Choose the wave type
+      osc.frequency.setValueAtTime(frequency, now + note.time);  // Set frequency
+      osc.connect(gainNode);  // Connect oscillator to gain node
+
+      osc.start(now + note.time);  // Start oscillator
+      osc.stop(now + note.time + note.duration);  // Stop after the note's duration
+    });
+  });
 }
 
 function onMIDISuccess(midi) {
@@ -169,6 +190,7 @@ function keyPressed() {
   if (key === 's') {
     show = !show;
     tmp = millis()
+    playMIDI(midi);
   }
   if (key === 'f' || key === 'F') {
     fileInput.elt.click();
@@ -206,77 +228,3 @@ async function handleFile(file) {
     console.log('Please upload a valid MIDI file.');
   }
 }
-
-// map midi keys from 35-81
-// ticks mean when the note starts
-// durationTicks means how long the note lasts 
-
-
-// Example MIDI track structure
-// {
-//   "name": "C Major Scale Test",
-//   "notes": [
-//     {
-//       "midi": 60,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 0,
-//       "durationTicks": 96
-//     },
-//     {
-//       "midi": 62,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 96,
-//       "durationTicks": 96
-//     },
-//     {
-//       "midi": 64,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 192,
-//       "durationTicks": 96
-//     },
-//     {
-//       "midi": 65,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 288,
-//       "durationTicks": 96
-//     },
-//     {
-//       "midi": 67,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 384,
-//       "durationTicks": 96
-//     },
-//     {
-//       "midi": 69,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 480,
-//       "durationTicks": 96
-//     },
-//     {
-//       "midi": 71,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 576,
-//       "durationTicks": 96
-//     },
-//     {
-//       "midi": 72,
-//       "velocity": 1,
-//       "noteOffVelocity": 0.5039370078740157,
-//       "ticks": 672,
-//       "durationTicks": 96
-//     }
-//   ],
-//   "pitchBends": [],
-//   "instrument": {
-//     "number": 0
-//   },
-//   "channel": 0,
-//   "endOfTrackTicks": 768
-// }
